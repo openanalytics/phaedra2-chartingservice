fe9ef214143574acbc39939d0e9bd38301180634
sberberovic <sasa.berberovic@openanalytics.eu>
2024-03-22 19:50:13 +0100

    fix: compilation error and clean code

--- server/src/main/java/eu/openanalytics/phaedra/chartingservice/api/ChartController.java
+++ server/src/main/java/eu/openanalytics/phaedra/chartingservice/api/ChartController.java
@@ -1,9 +1,10 @@
 package eu.openanalytics.phaedra.chartingservice.api;
 
 import eu.openanalytics.phaedra.chartingservice.enumeration.AxisFieldType;
+import eu.openanalytics.phaedra.chartingservice.exception.ChartDataException;
 import eu.openanalytics.phaedra.chartingservice.model.ChartData;
 import eu.openanalytics.phaedra.chartingservice.service.ChartDataService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/charts")
 public class ChartController {
 
-    @Autowired
-    private ChartDataService chartDataService;
+    private final ChartDataService chartDataService;
 
+    public ChartController(ChartDataService chartDataService) {
+        this.chartDataService = chartDataService;
+    }
+
+    @GetMapping("/scatter")
     public ChartData getScatterData(@RequestParam Long plateId, @RequestParam(required = false) Long protocolId,
                                     @RequestParam String xFieldName, @RequestParam AxisFieldType xFieldType,
-                                    @RequestParam String yFieldName, @RequestParam AxisFieldType yFieldType) {
+                                    @RequestParam String yFieldName, @RequestParam AxisFieldType yFieldType) throws ChartDataException {
         ChartData result = new ChartData();
         result.setXValues(chartDataService.getChartData(plateId, protocolId, xFieldName, xFieldType));
         result.setXValues(chartDataService.getChartData(plateId, protocolId, yFieldName, yFieldType));
